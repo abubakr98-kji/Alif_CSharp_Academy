cb5f6c5b9f09d01c2a7d3cf79369da12f08bc25a
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-08-07 11:18:02 +0500

    new changes

--- LogicalOperators/Program.cs
+++ LogicalOperators/Program.cs
@@ -8,24 +8,24 @@ namespace LogicalOperators
         static void Main(string[] args)
         {
             // Home Work 2
-            // int b = Convert.ToInt32(Console.ReadLine());
-            // int c = Convert.ToInt32(Console.ReadLine());
-            // if ((b>c) )
-            // {
-            //     b = b;
-            //     c = b;
-            //    Console.WriteLine($"Expression b: {b} , Expression c: {c}");
-            // }else if (b>c)
-            // {
-            //     b = c;
-            //     c = c;
-            //     Console.WriteLine($"Expression b: {b}, Expression c: {c}"); 
-            // }else if(b==c)
-            // {
-            //     b = 0;
-            //     c = 0;
-            //     Console.WriteLine(($"Expression b: {b}, Expression c: {c}"));
-            // }
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            if ((b>c) )
+            {
+                b = b;
+                c = b;
+               Console.WriteLine($"Expression b: {b} , Expression c: {c}");
+            }else if (b>c)
+            {
+                b = c;
+                c = c;
+                Console.WriteLine($"Expression b: {b}, Expression c: {c}"); 
+            }else if(b==c)
+            {
+                b = 0;
+                c = 0;
+                Console.WriteLine(($"Expression b: {b}, Expression c: {c}"));
+            }
 
             
 
@@ -85,28 +85,27 @@ namespace LogicalOperators
 
              // home work 4 , 2-nd way 
 
-             // int clinentEnter = Convert.ToInt32(Console.ReadLine() );
-             // // int checkNumber = clinentEnter;
-             // if ((clinentEnter == 0) ||  (clinentEnter <=14) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  0-14");
-             // }else if (clinentEnter < 0)
-             // {
-             //     Console.WriteLine("Number you wrote is not in interval  0-100???!");
-             //     
-             // }else if ((clinentEnter==15) ||  (clinentEnter<=35))
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  15-35");
-             // }else if ((clinentEnter==36) || (clinentEnter <= 50) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  36-50");
-             // }else if ((clinentEnter==50) || (clinentEnter<=100) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  50-100!!!!");
-             // }else if (clinentEnter>100)
-             // {
-             //     Console.WriteLine("Number is greater than  0-100!");
-             // }
+             int clinentEnter = Convert.ToInt32(Console.ReadLine() );
+             if ((clinentEnter == 0) ||  (clinentEnter <=14) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  0-14");
+             }else if (clinentEnter < 0)
+             {
+                 Console.WriteLine("Number you wrote is not in interval  0-100???!");
+                 
+             }else if ((clinentEnter==15) ||  (clinentEnter<=35))
+             {
+                 Console.WriteLine("Number you wrote is in interval  15-35");
+             }else if ((clinentEnter==36) || (clinentEnter <= 50) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  36-50");
+             }else if ((clinentEnter==50) || (clinentEnter<=100) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  50-100!!!!");
+             }else if (clinentEnter>100)
+             {
+                 Console.WriteLine("Number is greater than  0-100!");
+             }
 
 
         }
