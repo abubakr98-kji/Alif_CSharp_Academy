4db214ec6003f68e526b439059221dabd5e39eae
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-08-07 14:34:45 +0500

    changes in task 3

--- LogicalOperators/Program.cs
+++ LogicalOperators/Program.cs
@@ -27,47 +27,39 @@ namespace LogicalOperators
                 c = 0;
                 Console.WriteLine(($"Expression b: {b}, Expression c: {c}"));
             }
-
             
-
+            
             // Home Work 3
-             // int operand1 = int.Parse(Console.ReadLine());
-             // int operand2 = int.Parse(Console.ReadLine());
-             // string sign = Console.ReadLine();
-             // int divede0 = 
-             //
-             // switch (sign)
-             // {
-             //     case "+" :
-             //         Console.WriteLine(operand1+operand2);
-             //         break;
-             //     case "-":
-             //         Console.WriteLine(operand1-operand2);
-             //         break;
-             //     case "*":
-             //         Console.WriteLine(operand1*operand2);
-             //         break;
-             //     case "/": 
-             //         Console.WriteLine(operand1/operand2);
-             //         break;
-             //     default:
-             //         if (sign=="/" && (operand2==0))
-             //         {
-             //             Console.WriteLine("you wrong");
-             //         }
-             //         break;
-             //
-             // }
-
-          
+             int operand1 = int.Parse(Console.ReadLine());
+             int operand2 = int.Parse(Console.ReadLine());
+             string sign = Console.ReadLine();
              
-             // if (sign=="/" &&  operand2==0 )
-             // {
-             //     Console.WriteLine("you are wrong");        
-             // }
+             switch (sign)
+             {
+                 case "+" :
+                     Console.WriteLine(operand1+operand2);
+                     break;
+                 case "-":
+                     Console.WriteLine(operand1-operand2);
+                     break;
+                 case "*":
+                     Console.WriteLine(operand1*operand2);
+                     break;
+                 case "/": 
+                     if (  operand2==0 )
+                     {
+                         
+                         // Console.WriteLine(operand1/operand2 );
+                         Console.WriteLine("dont do it bro");
+                     }
+                     else
+                     {
+                        Console.WriteLine( operand1 / operand2);
+                     }
 
-
-
+                     break;
+             
+             }
 
              // Home Work 4
              // int enter = Convert.ToInt32(Console.ReadLine() );
@@ -95,27 +87,27 @@ namespace LogicalOperators
 
              // home work 4 , 2-nd way 
 
-             // int clinentEnter = Convert.ToInt32(Console.ReadLine() );
-             // if ((clinentEnter == 0) ||  (clinentEnter <=14) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  0-14");
-             // }else if (clinentEnter < 0)
-             // {
-             //     Console.WriteLine("Number you wrote is not in interval  0-100???!");
-             //     
-             // }else if ((clinentEnter==15) ||  (clinentEnter<=35))
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  15-35");
-             // }else if ((clinentEnter==36) || (clinentEnter <= 50) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  36-50");
-             // }else if ((clinentEnter==50) || (clinentEnter<=100) )
-             // {
-             //     Console.WriteLine("Number you wrote is in interval  50-100!!!!");
-             // }else if (clinentEnter>100)
-             // {
-             //     Console.WriteLine("Number is greater than  0-100!");
-             // }
+             int clinentEnter = Convert.ToInt32(Console.ReadLine() );
+             if ((clinentEnter == 0) ||  (clinentEnter <=14) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  0-14");
+             }else if (clinentEnter < 0)
+             {
+                 Console.WriteLine("Number you wrote is not in interval  0-100???!");
+                 
+             }else if ((clinentEnter==15) ||  (clinentEnter<=35))
+             {
+                 Console.WriteLine("Number you wrote is in interval  15-35");
+             }else if ((clinentEnter==36) || (clinentEnter <= 50) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  36-50");
+             }else if ((clinentEnter==50) || (clinentEnter<=100) )
+             {
+                 Console.WriteLine("Number you wrote is in interval  50-100!!!!");
+             }else if (clinentEnter>100)
+             {
+                 Console.WriteLine("Number is greater than  0-100!");
+             }
 
 
         }
