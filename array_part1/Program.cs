7a60eb4eaa1d7d29cdd3994ca4345968eca83798
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-08-15 13:22:49 +0500

    first commit

---
+++ array_part1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+// using.System.Linq.Enumerable.Sum();
+namespace array_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///home work 1.a
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // int b = Convert.ToInt32(Console.ReadLine());
+            //
+            // int sum = 0;
+            //
+            // for (int i = a+1; i < b; i++)
+            // {
+            //     sum += i;
+            //
+            //    Console.WriteLine($"Sum of numbers between a and b is:{sum}");
+            // }
+
+            ///home work 1.b
+
+            // int aa = Convert.ToInt32(Console.ReadLine());
+            // int bb = Convert.ToInt32(Console.ReadLine());
+            //
+            // for (int j = aa+1; j < bb; j ++)
+            // {
+            //     if (j%2==1)
+            //     {
+            //         Console.WriteLine(j);
+            //     }
+            // }
+
+            /////////home work 3 
+            /// Прямоугольник
+            string columns = "*";
+            string rows = "*";
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                Console.Write("*");
+                Console.Write(" ");
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.Write("\n*");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write("\n*");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write("\n*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write("*");
+                    Console.Write("\n");
+                   
+                }
+
+
+            }
+            
+            //// Прямоугольный трегольник
+
+            for (int t = 0; t < 1; t++)
+            {
+                Console.Write("*");
+                Console.Write("\n");
+                for (int m = 0; m < 1; m++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                    Console.Write(" ");
+                    Console.Write("*");
+                }
+            }
+
+        }
+
+    }
+}
+
+
