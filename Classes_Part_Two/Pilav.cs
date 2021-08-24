e3403f8a2eacd976188ce92b2785d3f5bc73ce9c
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-08-24 15:53:12 +0500

    first commit

---
+++ Classes_Part_Two/Pilav.cs
@@ -0,0 +1,14 @@
+﻿namespace Classes_Part_Two
+{
+    
+    
+    
+    public class Pilav
+    {
+        public string rice;
+        public string meat;
+        public string salat;
+        public int price;
+        public int size;
+    }
+}
---
+++ Classes_Part_Two/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classes_Part_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilav myPilav = new Pilav();
+            myPilav.meat = "Beef";
+            myPilav.rice = "long Pakistani";
+            myPilav.salat = "Alading";
+            myPilav.size = 1*1 ;
+            myPilav.price = 20;
+            
+            Console.WriteLine($"My Pilav consist of:\nMeat:{myPilav.meat}\nRice:{myPilav.rice}\nSalat:{myPilav.salat}\nSize:{myPilav.size}\nPrice:{myPilav.price}");
+        }
+    }
+}
