4e134b449a54bb8450d5aaebe39783c805def575
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-09-04 13:22:46 +0500

    Last chages i hope

--- array_part1/Program.cs
+++ array_part1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-// using.System.Linq.Enumerable.Sum();
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
 namespace array_part1
 {
     class Program
@@ -8,125 +10,158 @@ namespace array_part1
         {
 
             ///home work 1.a
-            // int a = Convert.ToInt32(Console.ReadLine());
-            // int b = Convert.ToInt32(Console.ReadLine());
-            //
-            // int sum = 0;
-            //
-            // for (int i = a+1; i < b; i++)
-            // {
-            //     sum += i;
-            //
-            //    Console.WriteLine($"Sum of numbers between a and b is:{sum}");
-            // }
+             int a = Convert.ToInt32(Console.ReadLine());
+             int b = Convert.ToInt32(Console.ReadLine());
+            
+             int sum = 0;
+            
+             for (int i = a+1; i < b; i++)
+             {
+                 sum += i;
+            
+                Console.WriteLine($"Sum of numbers between a and b is:{sum}");
+             }
 
-            ///home work 1.b
+            /home work 1.b
 
-            // int aa = Convert.ToInt32(Console.ReadLine());
-            // int bb = Convert.ToInt32(Console.ReadLine());
-            //
-            // for (int j = aa+1; j < bb; j ++)
-            // {
-            //     if (j%2==1)
-            //     {
-            //         Console.WriteLine(j);
-            //     }
-            // }
+             int aa = Convert.ToInt32(Console.ReadLine());
+             int bb = Convert.ToInt32(Console.ReadLine());
+            
+             for (int j = aa+1; j < bb; j ++)
+             {
+                 if (j%2==1)
+                 {
+                     Console.WriteLine(j);
+                 }
+             }
 
-            /////////home work 3 
-            /// Прямоугольник
-            string columns = "*";
-            string rows = "*";
-            for (int i = 0; i < 1; i++)
+
+
+            /*home work 3*/
+            /* Прямоугольник*/
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
             {
                 Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                Console.Write("*");
-                Console.Write(" ");
-                for (int j = 0; j < 1; j++)
+                for (int j = 0; j < num; j++)
                 {
-                    Console.Write("\n*");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
                     Console.Write("*");
-                    Console.Write("\n*");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write("\n*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write("*");
-                    Console.Write("\n");
-                   
                 }
+                Console.WriteLine();
+            } 
+             
 
+
+            /*Прямоугольный трегольник*/
+            for (int i = 0; i <= 7; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+            
+                Console.WriteLine();
+            }
+            
+            Console.Write("\n");
+            Console.Write("\n");
+            Console.Write("\n");
+            
+
+
+
+            /*Равностороний треугольник*/
+
+            int number, r, y, count = 1;
+            Console.Write("Enter number of rows:");
+            number = int.Parse(Console.ReadLine());
+            count = number - 1;
+            for (y = 1; y <= number; y++)
+            {
+                for (r = 1; r <= count; r++)
+                    Console.Write(" ");
+                count--;
+                for (r = 1; r <= 2 * y - 1; r++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+
+            /*Rhombus*/
+
+            Console.Write("Enter the maximum number of *: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nHere is the Diamond of Stars\n");
+            
+            for (int i = 1; i <= n; i++)
+            
+            {
+                for (int j = 0; j < (n - i); j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+                for (int k = 1; k < i; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+            
+                for (int j = 0; j < (n - i); j++)
+            
+                    Console.Write(" ");
+            
+                for (int j = 1; j <= i; j++)
+            
+                    Console.Write("*");
+            
+                for (int k = 1; k < i; k++)
+
+                        Console.Write("*");
+            
+                    Console.WriteLine();
+                }
+            
+                Console.WriteLine();
+            }
+
+
+            /*Home work 4*/
+            int first_Deposite = 1000;
+            int month = 12;
+            int max_value = 25;
+            int i;
+            int j;
+            
+            for (i = 1; i < max_value; i++)
+            {
+                for ( j= 1; j < month; j++)
+                {
+                    int calc =((first_Deposite*i)/100);
+                    first_Deposite+= calc;
+                    if (first_Deposite<=1100)
+                    {
+                        Console.WriteLine($"Month{j}/ Percantage {i} = {first_Deposite}");
+                    }
+                    else if (first_Deposite>1100)
+                    {
+                        first_Deposite = 1000;
+                        break;
+                    }
+
+                }
 
             }
             
-            //// Прямоугольный трегольник
-
-            for (int t = 0; t < 1; t++)
-            {
-                Console.Write("*");
-                Console.Write("\n");
-                for (int m = 0; m < 1; m++)
-                {
-                    Console.Write("*");
-                    Console.Write(" ");
-                    Console.Write(" ");
-                    Console.Write("*");
-                }
-            }
-
+            
+            
+            
         }
 
     }
 }
+
 
 
