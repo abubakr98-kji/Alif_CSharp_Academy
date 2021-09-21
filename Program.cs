2f3341989d8ae3fcf0b17ace7eeec4c545257e3f
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-09-21 21:34:48 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// using static OOP.Rectangle;
+
+
+namespace OOP
+{
+    
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+        double check = Convert.ToDouble(Console.ReadLine());
+        double check2 = Convert.ToDouble(Console.ReadLine());
+        
+        Rectangle myObj = new Rectangle(check,check2);
+          
+            
+            
+            Console.WriteLine(myObj.AreaCalculator());
+            Console.WriteLine(myObj.PerimeterCalculator());
+        }
+    
+ 
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Rectangle
+    {
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+
+        public double Area { get; }
+        public double Perimeter { get; }
+
+        public Rectangle(double _side1,double _side2)
+        {
+            side1 = _side1;
+            side2 = _side2;
+
+        }
+
+        public double AreaCalculator()
+        {
+            double calcArea = (side1* side2);
+            return calcArea;
+        }
+        
+        public double  PerimeterCalculator()
+        {
+             double calcPerimeter = 2 * (side1 + side2);
+             return calcPerimeter;
+        }
+        
+        
+      
+   
+       
+
+       
+
+    }
+}
