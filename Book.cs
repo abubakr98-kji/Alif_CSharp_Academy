01508ebefe45aaa339ecab4ad1782d0fb3f4f107
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-09-21 21:40:38 +0500

    first commit'

---
+++ Book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OOP2
+{
+    /*Start of Book*/
+    public class Book
+    {
+        
+        public Book()
+        {
+
+            
+        }
+    }
+    /*End of Book*/
+    
+    /*Begining of Title*/
+    public class Title
+    {
+        public string titleText { get; set; }
+        public Title(string _titleName)
+        {
+            titleText = _titleName;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"My Books Title name is: {titleText}");
+        }
+        
+        
+    }
+    /*End of Title*/
+    
+    public class Author
+    {
+        public string authorName { get; set; }
+        public Author(string _authorName)
+        {
+            authorName = _authorName; 
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Mt Books author name is: {authorName}");    
+        }
+    }
+    
+    /*Start of Content*/
+    public class Content
+    {
+        public string contentText { get; set; }
+        public Content(string _content)
+        {
+            contentText = _content;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Context of book: {contentText}");
+        }
+    }
+  /*End of Content*/
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+
+namespace OOP2
+{
+    class Program
+    {
+        
+        // public string authorName = Console.ReadLine();
+    
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string author = Console.ReadLine();
+            string content = Console.ReadLine();
+            
+            
+            Book myBook = new Book();
+
+            Author myAuthor = new Author(author);
+
+            Title myTitle = new Title(title);
+
+            Content myContent = new Content(content);
+
+            Console.WriteLine("By Book:");
+            myTitle.Show();
+            myAuthor.Show();
+            myContent.Show();
+
+
+        }
+    }
+}
