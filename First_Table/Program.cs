4f012ec8f9036eeb8fd8c648da912a1fa4515f39
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-09-22 20:30:24 +0500

    first commit

---
+++ First_Table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace First_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+
+
+        }
+    }
+}
---
+++ First_Table/Student.cs
@@ -0,0 +1,15 @@
+
+
+
+namespace First_Table
+{
+    
+    public class Student
+    {
+        public int StudendtID;
+        public string FirstName;
+        public string LastName;
+        public string HighSchool;
+        public int Birthday;
+    }
+}
