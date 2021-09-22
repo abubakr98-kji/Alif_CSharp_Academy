12c8f8d7610d797ec902fbb5a70e2f0dff412769
abubakr98-kji <abu.nazirmadov@gmail.com>
2021-09-22 20:33:06 +0500

    add sql script

---
+++ SQLQuery3.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE Students(
+	StudentID int identity primary key not null,
+	FirstName nvarchar(50) not null,
+	LastName nvarchar(50) not null,
+	HighSchool nvarchar(50) not null,
+	Birthday int not null,
+
+
+)
+
+
